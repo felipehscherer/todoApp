e69ab997faeadfb71960901e5c599c0dacd1b6e7
Felipe H. Scherer <109110173+felipehscherer@users.noreply.github.com>
2023-11-14 16:00:55 -0300

    Update ProjectDialogScreen.java

--- app/src/main/java/view/ProjectDialogScreen.java
+++ app/src/main/java/view/ProjectDialogScreen.java
@@ -1,17 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JDialog.java to edit this template
- */
 package view;
 
 import controller.ProjectController;
 import javax.swing.JOptionPane;
 import model.Project;
 
-/**
- *
- * @author Samsung
- */
 public class ProjectDialogScreen extends javax.swing.JDialog {
 
     ProjectController controller;
